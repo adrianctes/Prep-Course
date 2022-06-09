4679939e7aa8ac186131a0d198855cd54c6fe7fc
adrianctes <20182858@ipscorrientes.local>
2022-06-09 09:22:15 -0300

    04-JS-III completado

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -37,7 +37,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-  return array.push(elemento);
+  array.push(elemento);
+  return array;
 }
 
 
@@ -46,7 +47,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
-  return array.unshift(elemento);
+  array.unshift(elemento);
+  return array;
 }
 
 
@@ -117,12 +119,30 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  var respuesta=0;
+  if (typeof arguments[0] !== 'undefined') {
+      respuesta=1;
+       if (arguments.length > 1){
+        for(i=0; i < arguments.length; i++){
+              respuesta *= arguments[i];
+        }
+       }
+  }
+  return respuesta;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  var mayor=18;
+  var veces=0;
+  for(i=0; i < arreglo.length; i++){
+        if(arreglo[i]> mayor) {
+             veces+=1;
+        }       
+  } 
+  return veces;  
 
 }
 
@@ -132,6 +152,10 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+  if (numeroDeDia===1 || numeroDeDia===7) return "Es fin de semana";
+
+  return "Es dia Laboral";
+  
   
 } 
 
@@ -140,6 +164,10 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+
+  var n9= n.toString().charAt(0);
+  if (parseInt(n9) === 9) return true;
+  return false;
   
 }
 
@@ -148,6 +176,15 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
+  var elemento=arreglo[0];
+  var  value=true;
+  for(i=1; i < arreglo.length; i++){
+        if(arreglo[i]!== elemento) {
+             value=false;
+             break;
+        }       
+  } 
+  return value;  
   
 } 
 
@@ -157,13 +194,31 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
-}
+  var vector=[];
+  for(i=0; i < array.length; i++){
+        switch(array[i]){
+          case 'Enero':
+          case 'Marzo':
+          case 'Noviembre':
+                 vector.push(array[i]);  
+        }
+    }
+    return (vector.length === 3? vector : "No se encontraron los meses pedidos")   ;  
+} 
+
 
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var vector=[];
+  for(i=0; i < array.length; i++){
+       if (array[i]>100)     
+             vector.push(array[i]);  
+        }
+    return vector;      
+
 }
 
 
@@ -175,6 +230,22 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var vector=[];
+  var  nveces=0;
+  var leyenda="Se interrumpió la ejecución";
+  do {
+      nveces+=1;
+      numero+=2;
+      if (nveces === numero){
+            nveces=-1;
+            break;
+      }   
+      vector.push(numero);  
+     
+     }while (nveces < 10);
+  
+     return  (nveces=== -1? leyenda: vector)
+  
 }
 
 
@@ -185,6 +256,23 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var vector=[];
+  var  nveces=0;
+  do {
+      nveces+=1;
+     
+      if (nveces !== 5){
+        numero+=2; 
+        vector.push(numero);  
+      }   
+   
+     
+     }while (nveces < 10);
+  
+     return vector;  
+  
+
+
 }
 
 
